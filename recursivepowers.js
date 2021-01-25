3a8d66af6288a90497cd5e703f2bf2b44b83937f
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2021-01-25 18:20:57 +0100

    Create recursivepowers.js

---
+++ recursivepowers.js
@@ -0,0 +1,43 @@
+var isEven = function(n) {
+    return n % 2 === 0;
+};
+
+var isOdd = function(n) {
+    return !isEven(n);
+};
+
+var power = function(x, n) {
+    println("Computing " + x + " raised to power " + n + ".");
+    // base case
+    if(n === 0){
+        return 1;
+    }
+    // recursive case: n is negative 
+    if(n < 0){
+        return 1 / power(x, - n);
+    }
+    // recursive case: n is odd
+    if(isOdd(n) === true){
+        return x * power(x, n - 1);
+    }
+    // recursive case: n is even
+    if(isEven(n) === true){
+        var y = power(x, n/2);
+        return y * y;
+    }
+};
+
+var displayPower = function(x, n) {
+    println(x + " to the " + n + " is " + power(x, n));
+};
+
+displayPower(3, 0);
+Program.assertEqual(power(3, 0), 1);
+displayPower(3, 1);
+Program.assertEqual(power(3, 1), 3);
+displayPower(3, 2);
+Program.assertEqual(power(3, 2), 9);
+displayPower(3, -1);
+Program.assertEqual(power(3, -1), 1/3);
+Program.assertEqual(power(1, 1), 1);
+Program.assertEqual(power(2, 1), 2);
